55531e37b8a7354d81518b432c237d2dbf36d43f
Deniz Hadžirušidović <deniz-20114@outlook.com>
2023-06-11 04:51:50 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.his.healthinformationsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires javafx.swing;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+
+    opens com.his.healthinformationsystem to javafx.fxml;
+    exports com.his.healthinformationsystem;
+}
